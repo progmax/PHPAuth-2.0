3669f52382969ee900bd8e5f44dd586602e514d7
webot <olivier.debauche@gmail.com>
2012-12-28 10:20:28 +0100

    added index / latin1 -> utf8_bin : int

--- database.sql
+++ database.sql
@@ -21,8 +21,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `activations` (
   `uid` int(11) NOT NULL,
-  `activekey` varchar(20) CHARACTER SET latin1 NOT NULL,
-  `expiredate` datetime NOT NULL
+  `activekey` varchar(20) CHARACTER SET utf8_bin NOT NULL,
+  `expiredate` datetime NOT NULL,
+  KEY `activekey` (`activekey`),
+  KEY `uid` (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -32,9 +34,10 @@ CREATE TABLE IF NOT EXISTS `activations` (
 --
 
 CREATE TABLE IF NOT EXISTS `attempts` (
-  `ip` varchar(15) CHARACTER SET latin1 NOT NULL,
+  `ip` varchar(15) CHARACTER SET utf8_bin NOT NULL,
   `count` int(11) NOT NULL,
-  `expiredate` datetime NOT NULL
+  `expiredate` datetime NOT NULL,
+  KEY `ip` (`ip`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -45,10 +48,10 @@ CREATE TABLE IF NOT EXISTS `attempts` (
 
 CREATE TABLE IF NOT EXISTS `log` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(30) CHARACTER SET latin1 NOT NULL DEFAULT 'UNKNOWN' COMMENT 'Username or UID',
-  `action` varchar(100) CHARACTER SET latin1 NOT NULL,
-  `info` varchar(1000) CHARACTER SET latin1 NOT NULL DEFAULT 'None provided',
-  `ip` varchar(15) CHARACTER SET latin1 NOT NULL DEFAULT '0.0.0.0',
+  `username` varchar(30) CHARACTER SET urf8_bin NOT NULL DEFAULT 'UNKNOWN' COMMENT 'Username or UID',
+  `action` varchar(100) CHARACTER SET utf8_bin NOT NULL,
+  `info` varchar(1000) CHARACTER SET utf8_bin NOT NULL DEFAULT 'None provided',
+  `ip` varchar(15) CHARACTER SET utf8_bin NOT NULL DEFAULT '0.0.0.0',
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
@@ -61,8 +64,10 @@ CREATE TABLE IF NOT EXISTS `log` (
 
 CREATE TABLE IF NOT EXISTS `resets` (
   `uid` int(11) NOT NULL,
-  `resetkey` varchar(20) CHARACTER SET latin1 NOT NULL,
-  `expiredate` datetime NOT NULL
+  `resetkey` varchar(20) CHARACTER SET utf8_bin NOT NULL,
+  `expiredate` datetime NOT NULL,
+  KEY `uid` (`uid`),
+  KEY `resetkey` (`resetkey`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -74,11 +79,12 @@ CREATE TABLE IF NOT EXISTS `resets` (
 CREATE TABLE IF NOT EXISTS `sessions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `uid` int(11) NOT NULL,
-  `hash` varchar(32) CHARACTER SET latin1 NOT NULL,
+  `hash` varchar(32) CHARACTER SET utf8_bin NOT NULL,
   `expiredate` datetime NOT NULL,
-  `ip` varchar(15) CHARACTER SET latin1 NOT NULL,
-  `agent` varchar(200) CHARACTER SET latin1 NOT NULL,
-  PRIMARY KEY (`id`)
+  `ip` varchar(15) CHARACTER SET utf8_bin NOT NULL,
+  `agent` varchar(200) CHARACTER SET utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `hash` (`hash`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -89,11 +95,13 @@ CREATE TABLE IF NOT EXISTS `sessions` (
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(30) CHARACTER SET latin1 NOT NULL,
-  `password` varchar(128) CHARACTER SET latin1 NOT NULL,
-  `email` varchar(100) CHARACTER SET latin1 NOT NULL,
+  `username` varchar(30) CHARACTER SET utf8_bin NOT NULL,
+  `password` varchar(128) CHARACTER SET utf8_bin NOT NULL,
+  `email` varchar(100) CHARACTER SET utf8_bin NOT NULL,
   `isactive` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `email` (`email`),
+  KEY `username` (`username`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
