bc0497594e84db76b962dab6b772790dafc1429f
Cuonic <contact@cuonic.com>
2012-12-30 16:14:11 +0100

    Integrated Multilang, putLevel, sitekey, preparing passover to PSR-X Compliance

--- database.sql
+++ database.sql
@@ -76,6 +76,7 @@ CREATE TABLE IF NOT EXISTS `sessions` (
   `ip` varchar(15) COLLATE utf8_bin NOT NULL,
   `agent` varchar(200) COLLATE utf8_bin NOT NULL,
   `cookie_crc` varchar(40) COLLATE utf8_bin NOT NULL,
+  `lang` char(2) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`),
   KEY `hash` (`hash`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
@@ -92,6 +93,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(128) COLLATE utf8_bin NOT NULL,
   `email` varchar(100) COLLATE utf8_bin NOT NULL,
   `salt` varchar(20) COLLATE utf8_bin NOT NULL,
+  `lang` char(2) COLLATE utf8_bin NOT NULL DEFAULT 'en',
   `isactive` tinyint(1) NOT NULL DEFAULT '0',
   `level` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
