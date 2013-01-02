63b5b6afe7e2a7a589af4902e50a3cecfac6563a
webot <olivier.debauche@gmail.com>
2013-01-02 10:55:10 +0100

    Rollback
    Thanks Chris for the explanations

--- auth.class.php
+++ auth.class.php
@@ -3,42 +3,14 @@
 class Auth
 {
     private $dbh;
-    protected static $db;
 
     /*
     * Initiates database connection
     */
 
-    public function __construct()
+    public function __construct(PDO $dbh)
     {
-        $this->dbh = $Auth::getConnection();
-    }
-
-    /*
-    * Guarantees single instance, if no connection object exists then create one.
-    * @return object $db
-    */
-    
-    public static function getConnection() 
-    {
-        include("config.php");
-        
-        if (!self::$db) {
-            try {
-                // assign PDO object to db variable
-                self::$db = new PDO( 'mysql:host='.$db['host'].';dbname='.$db['name']."'", $db['user'], $db['pass'] );
-                self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-            }
-            catch (PDOException $e) 
-            {
-                //Output error - would normally log this to error file rather than output to user.
-                echo "Connection Error: " . $e->getMessage();
-            } 
-        }
-        else 
-        {        
-            return self::$db;
-        }
+        $this->dbh = $dbh;
     }
 
     /*
