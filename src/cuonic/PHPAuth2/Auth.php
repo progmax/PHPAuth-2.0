8ca3fc1a439869f75b865e7f2d12100f9e646747
webot <olivier.debauche@gmail.com>
2013-01-04 13:58:36 +0100

    Function getip : support of ipv6

--- src/cuonic/PHPAuth2/Auth.php
+++ src/cuonic/PHPAuth2/Auth.php
@@ -1578,12 +1578,18 @@ class Auth
     * @return string $ip
     */
 
+    // Source : http://stackoverflow.com/questions/1634782/what-is-the-most-accurate-way-to-retrieve-a-users-correct-ip-address-in-php?rq=1
+
     private function getIp()
     {
         foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key) {
             if (array_key_exists($key, $_SERVER) === true)  {
                 foreach (array_map('trim', explode(',', $_SERVER[$key])) as $ip) {
-                    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false) {
+                    if (filter_var($ip, FILTER_VALIDATE_IP, 
+                                        FILTER_FLAG_IPV4 | 
+                                        FILTER_FLAG_IPV6 |
+                                        FILTER_FLAG_NO_PRIV_RANGE | 
+                                        FILTER_FLAG_NO_RES_RANGE) !== false) {
                         return $ip;
                     }
                 }
